9663ff111d096d7ec599789bea740891db2ff3b2
Danilo Fernando Neves Fonseca <nevesdanilo290@gmail.com>
2024-05-03 09:27:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const addItemButton = document.getElementById('add-item');
     const calcularTotalButton = document.getElementById('calcular-total');
-    const resetarButton = document.getElementById('resetar');
+    const resetAll = document.getElementById('reset-all');
     const itemsContainer = document.getElementById('items-container');
     const resultado = document.getElementById('resultado');
 
@@ -27,12 +27,8 @@ document.addEventListener('DOMContentLoaded', function() {
         resultado.textContent = `Total: R$ ${total.toFixed(2)}`;
     });
 
-    // resetarButton.addEventListener('click', function() {
-    //     itemsContainer.innerHTML = ` 
-    //         <input type="number" class="quantidade" placeholder="Qtd">
-    //         <input type="text" class="nome" placeholder="Nome do item">
-    //         <input type="number" class="preco" placeholder="Preço"
-    //     `;
-    //     resultado.textContent = '';
-    // });
+    resetAllButton.addEventListener('click', function() {
+        itemsContainer.innerHTML = '';
+        resultado.textContent = '';
+    });
 });
